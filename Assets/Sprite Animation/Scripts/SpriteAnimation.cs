ab5b99ba0599583fb74c68eb0250f45bff66469c
braianhal <braianhal@gmail.com>
2016-07-31 20:11:20 -0300

    animation controller

---
+++ Assets/Sprite Animation/Scripts/SpriteAnimation.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class SpriteAnimation {
+
+    public string name;
+    public List<Sprite> sprites;
+
+}
---
+++ Assets/Sprite Animation/Scripts/SpriteAnimationController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteAnimationController : MonoBehaviour {
+
+    public List<SpriteAnimation> animations;
+
+    private SpriteRenderer render;
+
+    void Start()
+    {
+        render = this.GetComponent<SpriteRenderer>();
+    }
+
+}
